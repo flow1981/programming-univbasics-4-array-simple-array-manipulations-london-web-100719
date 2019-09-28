526e013cd1322be83a581dd41290aafaa59a6753
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-09-28 16:49:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array,string)
+  array.push(string);
+end
+
+def using_unshift(array, string)
+  array.unshift(string);
+end
+
+def using_pop(array)
+  array.pop;
+end
+
+def pop_with_args(array,num=2)
+  for (i=0,i<2,i++){
+    array.pop;
+  }
+  array
+end
+
+def using_shift(array)
+  array.shift;
+end
